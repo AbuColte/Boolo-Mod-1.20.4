83e9c0cff5212342a513692b3fd1a0ea31c28095
colte <rdaanyaal@gmail.com>
2024-05-21 22:57:10 +0200

    Initial commit

---
+++ src/main/java/net/colde/boolomod/BooloModClient.java
@@ -0,0 +1,10 @@
+package net.colde.boolomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BooloModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/colde/boolomod/BooloModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.colde.boolomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BooloModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
